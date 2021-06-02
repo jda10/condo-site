4cfcaab5e87eb17e62e6bbf622b9a5c99cbcb374
jda_10 <jatchley905@gmail.com>
2021-06-02 16:37:11 -0500

    Fixing typo

--- src/components/information.js
+++ src/components/information.js
@@ -8,7 +8,7 @@ class ContactInfo extends Component {
         return ( 
             <div id="information">
                 <h4 id="contact">Contact Details</h4>
-                <p><strong>Owner: Theresa Thomas</strong></p>
+                <p><strong>Owner: Teresa Thomas</strong></p>
             <div class="col">
                 <p>Phone</p>
                 <p>(423)-883-2742</p>
