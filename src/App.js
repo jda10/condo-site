f7942dc3cbedbc72af3b30f9088c40cef2d30286
jda_10 <jatchley905@gmail.com>
2021-06-02 14:11:56 -0500

    Commit of POC. Added condo images

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Gallery from './components/gallery';
 import Header from './components/header'
+import Contact from './components/information'
 import 'react-image-gallery/styles/css/image-gallery.css'
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Gallery></Gallery>
+      <Contact></Contact>
     </div>
   );
 }
--- src/components/information.js
+++ src/components/information.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
+import '../styles/info.css'
 
 class ContactInfo extends Component {
 
     render() { 
         return ( 
             <div id="information">
-                
+                <h4 id="contact">Contact Details</h4>
+                <p>Owner: Theresa Thomas</p>
+            <div class="col">
+                <p>Phone</p>
+                <p>(423)-883-2742</p>
+            </div>
+            <div class = "col">
+                <p>Email</p>
+                <a href="mailto:dalpal1997@gmail.com">dalpal1997@gmail.com</a>
+            </div>
             </div>
          );
     }
