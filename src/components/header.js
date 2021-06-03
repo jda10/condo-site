0672cf332458920e6a4bc79a736afe6fbc537b88
jda_10 <jatchley905@gmail.com>
2021-06-02 22:08:19 -0500

    Typo fix

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ class Header extends Component {
         return (
             <div id="header">
                 <p><strong>Edgewater 307 - Tower 3</strong></p>
-                <p>2 Bedroom / 2 Bath Condo</p>
+                <p>1 Bedroom / 1 Bath Condo</p>
                 <p>11212 Front Beach Rd, Panama City Beach, FL 32407</p>
             </div>
         );
