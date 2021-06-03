509d899247c0b51463d7cf2b28917c04f43c2213
jda_10 <jatchley905@gmail.com>
2021-06-02 19:33:18 -0500

    Added calling anchor

--- src/components/information.js
+++ src/components/information.js
@@ -11,7 +11,8 @@ class ContactInfo extends Component {
                 <p><strong>Owner: Teresa Thomas</strong></p>
             <div class="col">
                 <p>Phone</p>
-                <p>(423)-290-1847</p>
+                <a href="tel:4232901847">(423)-290-1847</a>
+                <p></p>
             </div>
             <div class = "col">
                 <p>Email</p>
