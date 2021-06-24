14790238ed4d37d2b31757216b69dd7b15f6dc97
jda_10 <jatchley905@gmail.com>
2021-06-24 13:51:08 -0500

    Removing outdated files. Moving pictures around

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -42,171 +42,171 @@ import image37 from '../imgs//condoImages/IMG_9130-min.jpg'
 
 
 const images = [
-    {
-      original: image1,
-      thumbnail: image1
-    },
-    {
-        original: image2,
-        thumbnail: image2
-      },
-      {
-        original: image3,
-        thumbnail: image3
-      },
-      {
-        original: image4,
-        thumbnail: image4
-      },
-      {
-        original: image5,
-        thumbnail: image5
-      },
-      {
-        original: image6,
-        thumbnail: image6
-      },
-      {
-        original: image7,
-        thumbnail: image7
-      },
-      {
-        original: image8,
-        thumbnail: image8
-      },
-      {
-        original: image9,
-        thumbnail: image9
-      },
-      {
-        original: image10,
-        thumbnail: image10
-      },
-      {
-        original: image11,
-        thumbnail: image11
-      },
-      {
-        original: image12,
-        thumbnail: image12
-      },
-      {
-        original: image13,
-        thumbnail: image13
-      },
-      {
-        original: image14,
-        thumbnail: image14
-      },
-      {
-        original: image15,
-        thumbnail: image15
-      },
-      {
-        original: image15,
-        thumbnail: image15
-      },
-      {
-        original: image16,
-        thumbnail: image16
-      },
-      {
-        original: image17,
-        thumbnail: image17
-      },
-      {
-        original: image18,
-        thumbnail: image18
-      },
-      {
-        original: image19,
-        thumbnail: image19
-      },
-      {
-        original: image20,
-        thumbnail: image20
-      },
-      {
-        original: image21,
-        thumbnail: image21
-      },
-      {
-        original: image22,
-        thumbnail: image22
-      },
-      {
-        original: image23,
-        thumbnail: image23
-      },
-      {
-        original: image24,
-        thumbnail: image24
-      },
-      {
-        original: image25,
-        thumbnail: image25
-      },
-      {
-        original: image26,
-        thumbnail: image26
-      },
-      {
-        original: image27,
-        thumbnail: image27
-      },
-      {
-        original: image28,
-        thumbnail: image28,
-      },
-      {
-        original: image29,
-        thumbnail: image29
-      },
-      {
-        original: image30,
-        thumbnail: image30
-      },
-    {
-        original: image31,
-        thumbnail: image31
-      },
-      {
-        original: image32,
-        thumbnail: image32
-      },
-      {
-        original: image33,
-        thumbnail: image33
-      },
-      {
-        original: image34,
-        thumbnail: image34
-      },
-      {
-        original: image35,
-        thumbnail: image35
-      },
-      {
-        original: image36,
-        thumbnail: image36
-      },
-      {
-        original: image37,
-        thumbnail: image37
-      },
-  ];
+  {
+    original: image1,
+    thumbnail: image1
+  },
+  {
+    original: image2,
+    thumbnail: image2
+  },
+  {
+    original: image3,
+    thumbnail: image3
+  },
+  {
+    original: image4,
+    thumbnail: image4
+  },
+  {
+    original: image5,
+    thumbnail: image5
+  },
+  {
+    original: image6,
+    thumbnail: image6
+  },
+  {
+    original: image7,
+    thumbnail: image7
+  },
+  {
+    original: image8,
+    thumbnail: image8
+  },
+  {
+    original: image9,
+    thumbnail: image9
+  },
+  {
+    original: image10,
+    thumbnail: image10
+  },
+  {
+    original: image11,
+    thumbnail: image11
+  },
+  {
+    original: image12,
+    thumbnail: image12
+  },
+  {
+    original: image13,
+    thumbnail: image13
+  },
+  {
+    original: image37,
+    thumbnail: image37
+  },
+  {
+    original: image14,
+    thumbnail: image14
+  },
+  {
+    original: image15,
+    thumbnail: image15
+  },
+  {
+    original: image15,
+    thumbnail: image15
+  },
+  {
+    original: image16,
+    thumbnail: image16
+  },
+  {
+    original: image17,
+    thumbnail: image17
+  },
+  {
+    original: image18,
+    thumbnail: image18
+  },
+  {
+    original: image19,
+    thumbnail: image19
+  },
+  {
+    original: image20,
+    thumbnail: image20
+  },
+  {
+    original: image21,
+    thumbnail: image21
+  },
+  {
+    original: image22,
+    thumbnail: image22
+  },
+  {
+    original: image23,
+    thumbnail: image23
+  },
+  {
+    original: image24,
+    thumbnail: image24
+  },
+  {
+    original: image25,
+    thumbnail: image25
+  },
+  {
+    original: image26,
+    thumbnail: image26
+  },
+  {
+    original: image27,
+    thumbnail: image27
+  },
+  {
+    original: image28,
+    thumbnail: image28,
+  },
+  {
+    original: image29,
+    thumbnail: image29
+  },
+  {
+    original: image30,
+    thumbnail: image30
+  },
+  {
+    original: image31,
+    thumbnail: image31
+  },
+  {
+    original: image32,
+    thumbnail: image32
+  },
+  {
+    original: image33,
+    thumbnail: image33
+  },
+  {
+    original: image34,
+    thumbnail: image34
+  },
+  {
+    original: image35,
+    thumbnail: image35
+  },
+  {
+    original: image36,
+    thumbnail: image36
+  }
+];
 
 class Gallery extends Component {
 
-    render() {
-        return <ImageGallery items={images}
-        showIndex={true}
-        originalWidth="75%"
-        originalHeight="75%"
-        thumbnailWidth="75%"
-        thumbnailHeight="75%"
-        />;
-      }
+  render() {
+    return <ImageGallery items={images}
+      showIndex={true}
+      originalWidth="75%"
+      originalHeight="75%"
+      thumbnailWidth="75%"
+      thumbnailHeight="75%"
+    />;
+  }
 }
- 
+
 export default Gallery;
